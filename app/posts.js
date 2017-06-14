d5fbbe4e33e47f6069bb74dafac7593569aa816c
Minh Dinh <dtminhpt@gmail.com>
2017-06-14 19:31:34 +0700

    Milestone1: Setup

---
+++ app/posts.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View, 
+  ListView
+} from 'react-native';
+
+
+export default class MyComponent extends Component {
+    constructor(props) {
+      super(props);
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.state = {
+        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      };
+    }
+
+    render() {
+      return (
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      );
+    }
+  }
